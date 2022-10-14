142947414b7ecdb123fd51e5a2c0c1d447472e5b
Goes Arik <53406260+goes-aric@users.noreply.github.com>
2022-10-14 15:00:59 +0800

    Update BaseService.php

--- app/Http/Services/BaseService.php
+++ app/Http/Services/BaseService.php
@@ -1,9 +1,11 @@
 <?php
 namespace App\Http\Services;
 
+use Exception;
 use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Hash;
@@ -48,16 +50,16 @@ class BaseService
         return $date;
     }
 
-    /* DATA FILTER AND RETURN AS A MODEL */
-    protected function dataFilter($model, $props){
+    /* DATA FILTER AND RETURN AS A MODEL WITH EXTENDED */
+    protected function dataFilter($model, $props, $extenders){
         /* SEARCH IF AVAILABLE */
-        if ($props['search'] != null) {
+        if (isset($props['search'])) {
             $model = $model->search($props['search'], null, true, true);
         }
 
         /* FILTER BY DATE IF AVAILABLE */
-        if ($props['start'] != null) {
-            if ($props['end'] != null) {
+        if (isset($props['start'])) {
+            if (isset($props['end'])) {
                 $model = $model->whereDate($props['date_field'], '>=', $this->returnDateOnly($props['start']))->whereDate($props['date_field'], '<=', $this->returnDateOnly($props['end']));
             } else {
                 $model = $model->whereDate($props['date_field'], '=', $this->returnDateOnly($props['start']));
@@ -65,43 +67,7 @@ class BaseService
         }
 
         /* LIMIT RECORDS */
-        if ($props['take'] != null) {
-            $model = $model->take($props['take']);
-        }
-        if (isset($props['skip'])) {
-            $model = $model->skip($props['skip'] * $props['take']);
-        }
-        if (isset($props['page'])) {
-            $model = $model->skip($props['page'] * $props['take']);
-        }
-
-        /* SORT RECORD IF AVAILABLE */
-        if ($props['sort_option'] != null && $props['sort_field']) {
-            $model = $model->orderBy($props['sort_field'], $props['sort_option']);
-        }
-
-        /* RETURN AS A MODEL */
-        return $model;
-    }
-
-    /* DATA FILTER AND RETURN AS A MODEL (EXTENDED) */
-    protected function dataFilterExtended($model, $props, $extenders){
-        /* SEARCH IF AVAILABLE */
-        if ($props['search'] != null) {
-            $model = $model->search($props['search'], null, true, true);
-        }
-
-        /* FILTER BY DATE IF AVAILABLE */
-        if ($props['start'] != null) {
-            if ($props['end'] != null) {
-                $model = $model->whereDate($props['date_field'], '>=', $this->returnDateOnly($props['start']))->whereDate($props['date_field'], '<=', $this->returnDateOnly($props['end']));
-            } else {
-                $model = $model->whereDate($props['date_field'], '=', $this->returnDateOnly($props['start']));
-            }
-        }
-
-        /* LIMIT RECORDS */
-        if ($props['take'] != null) {
+        if (isset($props['take'])) {
             $model = $model->take($props['take']);
         }
         if (isset($props['skip'])) {
@@ -112,12 +78,14 @@ class BaseService
         }
 
         /* EXTENDED SORT */
-        foreach ($extenders as $item) {
-            $model = $model->orderBy($item['field'], $item['option']);
+        if (isset($extenders)) {
+            foreach ($extenders as $item) {
+                $model = $model->orderBy($item['field'], $item['option']);
+            }
         }
 
         /* SORT RECORD IF AVAILABLE */
-        if ($props['sort_option'] != null && $props['sort_field']) {
+        if (isset($props['sort_option']) && isset($props['sort_field'])) {
             $model = $model->orderBy($props['sort_field'], $props['sort_option']);
         }
 
@@ -125,54 +93,32 @@ class BaseService
         return $model;
     }
 
-    /* DATA FILTER FOR PAGINATION AND RETURN AS A MODEL */
-    protected function dataFilterPagination($model, $props = []){
+    /* DATA FILTER FOR PAGINATION AND RETURN AS A MODEL WITH EXTENDED */
+    protected function dataFilterPagination($model, $props = [], $extenders){
+        // dd($props);
         /* SEARCH IF AVAILABLE */
-        if ($props['search'] != null) {
+        if (isset($props['search'])) {
             $model = $model->search($props['search'], null, true, true);
         }
 
         /* FILTER BY DATE IF AVAILABLE */
-        if ($props['start'] != null) {
-            if ($props['end'] != null) {
+        if (isset($props['start'])) {
+            if (isset($props['end'])) {
                 $model = $model->whereDate($props['date_field'], '>=', $this->returnDateOnly($props['start']))->whereDate($props['date_field'], '<=', $this->returnDateOnly($props['end']));
             } else {
                 $model = $model->whereDate($props['date_field'], '=', $this->returnDateOnly($props['start']));
             }
         }
 
-        /* SORT RECORD IF AVAILABLE */
-        if ($props['sort_option'] != null && $props['sort_field']) {
-            $model = $model->orderBy($props['sort_field'], $props['sort_option']);
-        }
-
-        /* RETURN AS A MODEL */
-        return $model;
-    }
-
-    /* DATA FILTER FOR PAGINATION AND RETURN AS A MODEL (EXTENDED) */
-    protected function dataFilterPaginationExtended($model, $props = [], $extenders){
-        /* SEARCH IF AVAILABLE */
-        if ($props['search'] != null) {
-            $model = $model->search($props['search'], null, true, true);
-        }
-
-        /* FILTER BY DATE IF AVAILABLE */
-        if ($props['start'] != null) {
-            if ($props['end'] != null) {
-                $model = $model->whereDate($props['date_field'], '>=', $props['start'])->whereDate($props['date_field'], '<=', $props['end']);
-            } else {
-                $model = $model->whereDate($props['date_field'], '=', $props['start']);
+        /* EXTENDED SORT */
+        if (isset($extenders)) {
+            foreach ($extenders as $item) {
+                $model = $model->orderBy($item['field'], $item['option']);
             }
         }
 
-        /* EXTENDED SORT */
-        foreach ($extenders as $item) {
-            $model = $model->orderBy($item['field'], $item['option']);
-        }
-
         /* SORT RECORD IF AVAILABLE */
-        if ($props['sort_option'] != null && $props['sort_field']) {
+        if (isset($props['sort_option']) && isset($props['sort_field'])) {
             $model = $model->orderBy($props['sort_field'], $props['sort_option']);
         }
 
@@ -180,6 +126,7 @@ class BaseService
         return $model;
     }
 
+    /* UPLOAD FILE FUNCTION */
     protected function returnUploadFile($path, $props)
     {
         try {
@@ -208,12 +155,12 @@ class BaseService
 
             $response = [
                 'status'        => 'success',
-                'status_code'   => 201,
+                'status_code'   => Response::HTTP_CREATED,
                 'filename'      => $newName,
                 'message'       => 'Image uploaded'
             ];
             return $response;
-        } catch (\Exception $ex) {
+        } catch (Exception $ex) {
             $response = [
                 'status'        => 'error',
                 'status_code'   => $ex->getCode(),
@@ -223,23 +170,18 @@ class BaseService
         }
     }
 
+    /* DELETE FILE FUNCTION */
     protected function returnDeleteFile($path, $filename)
     {
         /* DELETE IMAGE IF EXISTS ON STORAGE */
-        $path = $path . '/' . $filename;
-        if(Storage::exists($path)){
-            File::delete($path);
-        }
-    }
+        $originalPath = $path . '/' . $filename;
+        $thumbnailPath = $path . '/thumbnail/' . $filename;
 
-    protected function returnCopyFile($path, $from, $to)
-    {
-        /* DELETE IMAGE IF EXISTS ON STORAGE */
-        $fromPath = $path . '/' . $from;
-        $toPath = $path . '/' . $to;
-
-        if(Storage::exists($path)){
-            File::copy($fromPath, $toPath);
+        try {
+            File::delete($originalPath);
+            File::delete($thumbnailPath);
+        } catch (Exception $ex) {
+            return $ex->getMessage();
         }
     }
 }
